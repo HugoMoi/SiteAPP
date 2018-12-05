fc9c651af38c44ae5b3c4c0124d8b45167c10b4b
HugoMoi <45209254+HugoMoi@users.noreply.github.com>
2018-12-05 11:23:04 +0100

    version final

--- FAQ.php
+++ FAQ.php
@@ -5,8 +5,8 @@ $messages = $bdd->query('SELECT * FROM post');?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>FAQ</title>
-	 <meta charset="utf-8">
+    <title>FAQ</title>
+     <meta charset="utf-8">
     <link rel="stylesheet" href="../Style/FAQ.css" />
     <link rel="stylesheet" href="../Style/General.css" />
 
@@ -19,131 +19,34 @@ $messages = $bdd->query('SELECT * FROM post');?>
 <h1>FAQ</h1>
 <h2>Les questions les plus fréquentes</h2>
 
-<section class="question">
-    <input type="checkbox" id="Question1">
-    <label for="Question1"> <?php 
-    	$id = 1;
-    	$test = $bdd->prepare('SELECT Question FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Question'];?>	
-    	</label>
+<?php $compteur=0;
+    while($post = $messages->fetch()){ ?>
+       <?php $compteur++;?>
 
-    	<p> <?php 
-    	$id = 1;
-    	$test = $bdd->prepare('SELECT Reponse FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo htmlspecialchars($message['Reponse']);?>
-    	
-    	</p>
-	</section>
+    <section class="question">
+        <input type="checkbox" id="=<?php echo $compteur; ?>">
+        
+        <!-- la question -->
+        <label for="=<?php echo $compteur; ?>"> 
+            <?php 
+            $id = $compteur;
+            $test = $bdd->prepare('SELECT Question FROM post WHERE id = ?');
+            $test -> execute(array($id));
+            echo $post['Question']; ?>
+        </label>
 
+        <!--la réponse  -->
+        <p>  
+            <?php $id = $compteur;
+            $test = $bdd->prepare('SELECT Reponse FROM post WHERE id = ?');
+            $test -> execute(array($id));
+            echo $post['Reponse'];
+            ?>
+        </p>
+    </section>
+    <?php
+    }?>
 
-<section class="question">
-    <input type="checkbox" id="Question2">
-    <label for="Question2">
-    	<?php
-    	$id = 2;
-    	$test = $bdd->prepare('SELECT Question FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Question'];?>
-    	</label>
-
-    <p> <?php 
-    	$id = 2;
-    	$test = $bdd->prepare('SELECT Reponse FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Reponse'];?></p>
-</section>
-
-
-<section class="question">
-    <input type="checkbox" id="3">
-    <label for="3">
-    	<?php
-    	$id = 3;
-    	$test = $bdd->prepare('SELECT Question FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Question'];?>
-    	</label>
-
-    <p><?php 
-    	$id = 3;
-    	$test = $bdd->prepare('SELECT Reponse FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Reponse'];?></p>
-</section>
-
-
-<section class="question">
-    <input type="checkbox" id="Question4">
-    <label for="Question4">
-    	<?php
-    	$id = 4;
-    	$test = $bdd->prepare('SELECT Question FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Question'];?>
-    	</label>
-
-    <p><?php 
-    	$id = 4;
-    	$test = $bdd->prepare('SELECT Reponse FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Reponse'];?></p>
-</section>
-
-<section class="question">
-    <input type="checkbox" id="Question5">
-    <label for="Question5"><?php
-    	$id = 5;
-    	$test = $bdd->prepare('SELECT Question FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Question'];?></label>
-    <p><?php 
-    	$id = 5;
-    	$test = $bdd->prepare('SELECT Reponse FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Reponse'];?></p>
-</section>
-
-<section class="question">
-    <input type="checkbox" id="Question6">
-    <label for="Question6"><?php
-    	$id = 6;
-    	$test = $bdd->prepare('SELECT Question FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Question'];?>
-    	</label>
-
-    <p><?php 
-    	$id = 6;
-    	$test = $bdd->prepare('SELECT Reponse FROM post WHERE id = ?');
-    	$test -> execute(array($id));
-    	$message = $test->fetch();
-    	?>
-    	<?php echo $message['Reponse'];?></p>
-</section>
 
 <footer>Si vous n'avez pas trouvé de réponse à votre question vous pouvez regader sur le forum  ou alors nous contacter directement</footer>
 </body>

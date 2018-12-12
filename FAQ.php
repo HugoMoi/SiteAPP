2dccd5d8870fde964f823a8881afa60f74a81903
HugoMoi <45209254+HugoMoi@users.noreply.github.com>
2018-12-12 11:06:52 +0100

    avec une fonction

--- FAQ.php
+++ FAQ.php
@@ -1,14 +1,22 @@
 <?php 
-$bdd = new PDO("mysql:host=localhost;dbname=faq",'root','');
-$messages = $bdd->query('SELECT * FROM post');?>
+$bdd = new PDO("mysql:host=localhost;dbname=espace_membre",'root','');
+$messages = $bdd->query('SELECT * FROM post');
 
+    function faq($compteur){    
+        $bdd = new PDO("mysql:host=localhost;dbname=espace_membre",'root','');
+        $messages = $bdd->prepare('SELECT * FROM post WHERE id = ?');
+        $messages->execute(array($compteur));
+        $post = $messages->fetch();
+    } 
+
+    
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>FAQ</title>
      <meta charset="utf-8">
     <link rel="stylesheet" href="../Style/FAQ.css" />
-    <link rel="stylesheet" href="../Style/General.css" />
 
 </head>
 <body>
@@ -18,35 +26,32 @@ $messages = $bdd->query('SELECT * FROM post');?>
 
 <h1>FAQ</h1>
 <h2>Les questions les plus fréquentes</h2>
-
+<div class="faq">
 <?php $compteur=0;
-    while($post = $messages->fetch()){ ?>
-       <?php $compteur++;?>
+    while($post = $messages->fetch()){ 
+       $compteur++;?>
 
     <section class="question">
         <input type="checkbox" id="=<?php echo $compteur; ?>">
         
         <!-- la question -->
         <label for="=<?php echo $compteur; ?>"> 
-            <?php 
-            $id = $compteur;
-            $test = $bdd->prepare('SELECT Question FROM post WHERE id = ?');
-            $test -> execute(array($id));
-            echo $post['Question']; ?>
+           <?php faq($compteur); 
+                   echo $post['Question'];
+?>
         </label>
 
         <!--la réponse  -->
         <p>  
-            <?php $id = $compteur;
-            $test = $bdd->prepare('SELECT Reponse FROM post WHERE id = ?');
-            $test -> execute(array($id));
-            echo $post['Reponse'];
-            ?>
+            <?php faq($compteur); 
+                    echo $post['Reponse'];
+?>
+
         </p>
     </section>
     <?php
     }?>
-
+</div>
 
 <footer>Si vous n'avez pas trouvé de réponse à votre question vous pouvez regader sur le forum  ou alors nous contacter directement</footer>
 </body>

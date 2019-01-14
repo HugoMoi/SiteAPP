d1ba3184944515ae91ab0133b2a8149ab0760e2d
HugoMoi <45209254+HugoMoi@users.noreply.github.com>
2019-01-14 16:36:33 +0100

    avec description du site

--- forum/views/Accueil.php
+++ forum/views/Accueil.php
@@ -16,6 +16,23 @@
 
 	<div class="Apropos">
 		<h1>Venez souscrire à notre offre</h1>
+
+	<div class="PresentationGris">
+		<img class="img" src="image/img1.png" alt="img1"/>
+		<p> Inscrivez-très facilement et commandez vos capteurs pour contrôler votre maison. Vous les ajoutez facilement à votre compte.</p>
 	</div>
-</body>
+
+	<div class="PresentationBlanc">
+		<img class="img" src="image/img2.png" alt="img1"/>
+		<p> Contrôler vos appareils connectés grâce à la page 'Maison' où tous vos appreils apparaissent. Un simple clic vous permet d'allumer ou éteindre une lampe, ou encore règler votre chauffage.</p>
+		<br><br>
+	</div>
+
+	<div class="PresentationGris">
+		<img class="img" src="image/img3.png" alt="img1"/>
+		<p> Le forum permet de poser toutes les questions que vous pouvez vous poser. La communauté vous répondra avec plaisir et certains auront peut être rencontré le même problème que vous.</p>
+		<br><br>
+	</div>
+</div>
+<br><br></body>
 </html>

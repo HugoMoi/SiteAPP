3b71da3b0005c09af5195ee76386d9d3e54a286e
HugoMoi <45209254+HugoMoi@users.noreply.github.com>
2019-01-14 17:59:47 +0100

    partie de chuanhan
    dans le general.php ajouter le lien 
    ajouter le case "statistique" dans l'index
    ajouter la fonction du controller

---
+++ forum/views/diagramme_humite.php
@@ -0,0 +1,75 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>diagramme humidité</title>
+</head>
+<body>
+
+<script src="https://code.highcharts.com/highcharts.js"></script>
+
+    <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+
+<?php
+    $servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "espace_membre";
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $result=$conn->query('SELECT date,humite FROM dbStatistique');
+    while($row=$result->fetch()){
+        $day[]=$row['date'];
+        $pourcent[]=$row['humite'];
+    }
+    $day=json_encode($day);
+    //print($day);
+    $pourcent=json_encode($pourcent,JSON_NUMERIC_CHECK);//后缀定义讲字串符转化为数字
+    //print($number);
+    $conn = null;
+?>
+
+<script>
+Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'humidité'
+    },
+    xAxis: {
+        categories: <?php echo $day;?>,
+    },
+    yAxis: {
+        title: {
+            text: '%'
+        },
+        labels: {
+            formatter: function () {
+                return this.value;
+            }
+        }
+    },
+    tooltip: {
+        split: true,
+        valueSuffix: '%'
+    },
+    plotOptions: {
+        area: {
+            stacking: 'normal',
+            lineColor: '#666666',
+            lineWidth: 1,
+            marker: {
+                lineWidth: 1,
+                lineColor: '#666666'
+            }
+        }
+    },
+    series: [{
+        name: 'humidité',
+        data: <?php echo $pourcent; ?>,
+    }]
+});
+</script>
+</body>
+</html>
